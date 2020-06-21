be57f01031c12bbb707f45490ee577fe75140722
lay0020 <lay0020@gmail.com>
2020-06-21 16:11:52 +0800

    overflow-溢出設定

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,16 @@
-$('#banner').slick();
-$('#brand').slick();
+Aos.init();
+
+$('#banner').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false
+});
+$('#brand').slick({
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    dots: true,
+    variableWidth:true,
+});
